6718a248b6f6fda1f7ae716c59e459f4b6bd7b1b
heidiyf <heidiyf29@gmail.com>
2023-09-11 16:07:50 -0400

    refactored to extract helper method

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -3,12 +3,10 @@
  */
 class FizzBuzz {
     public static void main(String[] args) {
-
         int i = 1;
         while (i < 100) {
             i = doFizzBuzz(i);
         }
-
     }
 
     private static int doFizzBuzz(int i) {
